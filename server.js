806b73c81d8be9e682f1db353d16405d6a56d0f1
L0 <jana16.kahld@gmail.com>
2024-10-24 00:13:37 +0300

    test

--- server.js
+++ server.js
@@ -54,4 +54,51 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
+// if using synchronous loading, will be called once the DOM is ready
+turnstile.ready(function () {
+  turnstile.render("#myWidget", {
+    sitekey: "0x4AAAAAAAyPW0WrShoxyuG",
+    callback: function (token) {
+      console.log(`Challenge Success ${token}`);
+    },
+  });
+});
 
+
+
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/login', async (req, res) => {
+    const turnstileToken = req.body['cf-turnstile-response'];
+    const secretKey = '0x4AAAAAAAyPW0ZlI4Ltxv2qhTdXwdsm5uw';
+
+    try {
+        const response = await axios.post('https://challenges.cloudflare.com/turnstile/v0/siteverify', null, {
+            params: {
+                secret: secretKey,
+                response: turnstileToken
+            }
+        });
+
+        if (response.data.success) {
+            // Token is valid, proceed with login
+            const username = req.body.username;
+            const password = req.body.password;
+            // Your login logic here
+            res.send('Login successful');
+        } else {
+            res.status(400).send('Turnstile verification failed');
+        }
+    } catch (error) {
+        res.status(500).send('Error verifying Turnstile token');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
