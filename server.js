f4708c93712e2bc099dbb67e89c476a7f6d5e816
L0 <jana16.kahld@gmail.com>
2024-10-24 00:56:05 +0300

    test

--- server.js
+++ server.js
@@ -29,6 +29,37 @@ app.get('/login', (req, res) => {
 });
 
 
+
+
+
+
+const token = form.data['cf-turnstile-response']; // الحصول على التوكن من النموذج
+const cfFormData = new FormData();
+
+// إضافة المفاتيح إلى FormData
+cfFormData.append('secret', '0x4AAAAAAAyPW0ZlI4Ltxv2qhTdXwdsm5uwy'); // استبدل 'your-secret-key' بمفتاحك السري الفعلي
+cfFormData.append('response', token);
+
+const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify'; // رابط التحقق
+
+// إرسال طلب التحقق إلى Cloudflare
+const result = await fetch(url, {
+  body: cfFormData,
+  method: 'POST'
+});
+
+// الحصول على الاستجابة من Cloudflare
+const outcome = await result.json();
+
+// التحقق من نجاح أو فشل التوكن
+if (!outcome.success) {
+  throw new Error('Cloudflare JS was not completed correctly!'); // رمي خطأ في حال فشل التحقق
+}
+
+
+
+
+
 app.get('/submitSignup', (req, res) => {
 
   // getting the username and password from the query string
@@ -37,6 +68,7 @@ app.get('/submitSignup', (req, res) => {
     username : username,
     password : password
   }
+
   // databe call to create user
   database.signup(user)
   .then((result) => {
